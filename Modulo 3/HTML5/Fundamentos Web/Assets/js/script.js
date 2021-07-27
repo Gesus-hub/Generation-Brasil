8ab8df552300d57b6afce10a22723b66f925ab04
Gesus-hub <guimaraes.viniciusdasilva@gmail.com>
2021-07-27 01:20:25 -0300

    Angular

---
+++ Modulo 3/HTML5/Fundamentos Web/Assets/js/script.js
@@ -0,0 +1,42 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome invalido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome valido'
+        txtNome.style.color='green'
+        let nomeOk = true
+}
+
+function validaEmail(){
+    let txt = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'E-mail invalido'
+        txtEmail.style.color = 'red'
+    } else {
+        textEmail.innerHTML='E-mail valido'
+        txtEmail.style.color='green '
+        let emailOk = true
+    }
+}
+
+function enviar (){
+    if(nomeOk == true && emailOk == true){
+        alert('Formulario enviado com sucesso!')
+
+    }else{
+        alert('Preencha o formulario corretamente antes de enviar!')
+    }
+}
